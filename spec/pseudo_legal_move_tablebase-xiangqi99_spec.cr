314707c6b026c8bce4fabb8df4863b54eb507d63
Cyril Kato <contact@cyril.email>
2020-10-08 11:19:13 +0200

    test(DB): sort keys before comparaison

--- spec/pseudo_legal_move_tablebase-xiangqi99_spec.cr
+++ spec/pseudo_legal_move_tablebase-xiangqi99_spec.cr
@@ -2,6 +2,23 @@ require "./spec_helper"
 
 describe PseudoLegalMoveTablebase::Xiangqi99 do
   it "includes Xiangqi99 pieces" do
-    PseudoLegalMoveTablebase::Xiangqi99::DB.keys.should eq(["X:-K", "X:A", "X:C", "X:E", "X:H", "X:R", "X:S", "x:-k", "x:a", "x:c", "x:e", "x:h", "x:r", "x:s"])
+    PseudoLegalMoveTablebase::Xiangqi99::DB.keys.sort.should eq(
+      %w(
+        X:-K
+        X:A
+        X:C
+        X:E
+        X:H
+        X:R
+        X:S
+        x:-k
+        x:a
+        x:c
+        x:e
+        x:h
+        x:r
+        x:s
+      ).sort
+    )
   end
 end
